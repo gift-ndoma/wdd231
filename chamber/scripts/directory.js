17f4a384a736637ba35e1efc749aa0cbbfd1be38
gift-ndoma <giftndoma87@gmail.com>
2025-05-16 10:27:55 +0100

    initial commit

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const url = "https://github.com/gift-ndoma/wdd231/blob/main/chamber/data/members.json";
+const url = "../chamber/data/members.json";
 const cards = document.getElementById('cards');
 
 async function getMembersData() {
@@ -15,19 +15,51 @@ const displayMembers = (members) => {
         let fullName = document.createElement("h2");
         let portrait = document.createElement("img");
         let website = document.createElement("p");
+        let address = document.createElement("p");
+        let description = document.createElement("p");
+
+        website.classList.add("link")
+        description.classList.add("description")
 
         fullName.innerHTML = `${member.name}`;
-        website.innerHTML = `${member.website}`
+        website.innerHTML = `${member.website}`;
+        address.innerHTML = `${member.address}`;
+        description.innerHTML = `${member.description}`;
 
-        portrait.setAttribute("src", member.imageurl);
+        portrait.setAttribute("src", member.image);
         portrait.setAttribute("alt", `Logo of ${member.name}.`);
         portrait.setAttribute("loading", "lazy");
-        portrait.setAttribute("width", "300");
-        portrait.setAttribute("height", "300");
+        portrait.setAttribute("width", "70");
+        portrait.setAttribute("height", "70");
 
-        card.appendChild(fullName);
         card.appendChild(portrait);
+        card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(website);
+        card.appendChild(description);
 
         cards.appendChild(card)
     });
 }
+
+const gridButton = document.getElementById("grid");
+const listButton = document.getElementById("list");
+
+gridButton.addEventListener("click", () => {
+    cards.classList.add("grid");
+
+    cards.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    cards.classList.add("list");
+    cards.classList.remove("grid")
+});
+
+const hamButton = document.getElementById("hamButton");
+const navLinks = document.getElementById("navLinks");
+
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("active");
+    navLinks.classList.toggle("active");
+})
